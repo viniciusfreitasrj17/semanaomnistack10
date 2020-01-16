2a2d7b47507563d8f75b3112e2b4513f0f841121
Vinicius Freitas <viniciusfreitasrj17@gmail.com>
2020-01-15 23:05:46 -0300

    mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const monsoose = require('mongoose');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello OmniStack !!!' });
+monsoose.connect('mongodb+srv://marcos:l4cun4@cluster0-hnlwd.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
+
+app.use(express.json())
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+// Query Params: request.query (filtros, ordenação, paginação, ...)
+// Route Params: request.params (identificar um recurso na açteração ou remoção)
+// Body: request.body (dados para criação ou alteração de um registro)
+
+app.get('/', (request, response) => {
+    const m = request.query;
+    return response.json({ message: 'Hello OmniStack !!!', query: m });
+});
+
+app.delete('/:id', (request, response) => {
+    const d = request.params.id;
+    return response.json({ deleted: d });
+})
+
+app.post('/', (request, response) => {
+    const b = request.body;
+    return response.json({ posting: b });
+})
 
 app.listen(3333);
